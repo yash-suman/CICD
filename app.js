8f2c5cda8d4b75a0e16c9a70903bc5ac44b24f57
karthik <38027604+venkatakarthik94@users.noreply.github.com>
2021-07-16 15:39:57 +0530

    Update app.js
    test commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('App is running '));
+app.get('/', (req, res) => res.send('App is running fine '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
