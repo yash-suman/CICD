6575b4aabe317145452409bc5c4a06b227b906c5
karthik <38027604+venkatakarthik94@users.noreply.github.com>
2021-08-09 19:12:36 +0530

    Update app.js
    My todays changes for new project

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8081;
 
-app.get('/', (req, res) => res.send('App is running fine with my code and i edited it'));
+app.get('/', (req, res) => res.send('App is running fine'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
