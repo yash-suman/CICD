05183e0dae4576c81df9a49c998f345869a23fe7
yash-suman <88178234+yash-suman@users.noreply.github.com>
2021-08-10 16:55:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8081;
 
-app.get('/', (req, res) => res.send('App is running fine on  Docker container'));
+app.get('/', (req, res) => res.send('Test App is running fine on  Docker container'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
